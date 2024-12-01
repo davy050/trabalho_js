f124a549dfcff6dd355de63cdfa9f8fa97e19716
davy araujo <davyaraujo0506@gmail.com>
2024-12-01 14:40:20 -0300

    exercicio_html_js

---
+++ main.js
@@ -0,0 +1,12 @@
+document.getElementById("form-deposito").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const numeroA = parseFloat(document.getElementById("maisAlto").value);
+    const numeroB = parseFloat(document.getElementById("maisBaixo").value);
+
+    if (numeroA > numeroB) {
+        alert("Número A é maior que número B. Tudo certo!");
+    } else {
+        alert("Erro: O número A deve ser maior que o número B.");
+    }
+});
